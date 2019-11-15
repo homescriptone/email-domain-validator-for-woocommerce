d6f95bb3604bfd8ffc8c3beebe6c768f0df48340
Manu The Blacker <emmanueladekplovi1@gmail.com>
2019-11-15 09:33:03 +0100

    updating the plugin and option name

--- hs-ev.php
+++ hs-ev.php
@@ -1,8 +1,8 @@
 <?php
 /**
- * Plugin Name:       WooCommerce Checkout Email Validator
+ * Plugin Name:       Email Domain Validator for WooCommerce
  * Plugin URI:        https://homescript1.github.io/once-cart-items-remover/
- * Description:        An easy tool for prevent email spamming on your store.
+ * Description:       An easy tool for prevent email spamming on your store.
  * Version:           1.0
  * Author:            HomeScript
  * Author URI:        https://homescriptone.com
@@ -22,13 +22,18 @@ require_once HS_EV_PLUGIN_PATH.'homescript_fields.php';
 
 
 add_action('admin_notices','hs_check_wc');
+/**
+ * This function check if WooCommerce is installed.
+ *
+ * @return void
+ */
 function hs_check_wc(){
     if (!class_exists('WooCommerce') ){
         ?>
             <div class=" notice notice-error">
                 <p>
                 <?php
-                esc_html_e( 'WooCommerce Checkout Email Validator require WooCommerce for work.', 'hs_ev' );
+                esc_html_e( 'Email Domain Validator for WooCommerce require WooCommerce for work.', 'hs_ev' );
                 ?>
                  </p>
             </div>
@@ -37,11 +42,21 @@ function hs_check_wc(){
 }
 
 add_action( 'woocommerce_get_sections_email' , 'hs_ev_options',10,1 );
+/**
+ * This function is responsible to add a new settings into WooCommerce Settings.
+ *
+ * @param array $settings
+ * @return void
+ */
 function hs_ev_options($settings){
-   $settings['hs_ev']= __( 'WooCommerce Checkout Email Validator','hs_ev');
+   $settings['hs_ev']= __( 'Email Domain Validator for WooCommerce','hs_ev');
    return $settings;
 }
+
 add_action( 'woocommerce_settings_tabs_email' , 'hs_ev_settings');
+/**
+ * This function render the fields into WooCommerce Settings.
+ */
 function hs_ev_settings( ) {
    
     $value = get_option('hs_ev_enabled');
@@ -51,8 +66,8 @@ function hs_ev_settings( ) {
         homescript_input_fields('enable_hs_ev',
             array(
                 'type'        => 'checkbox',
-                'label' => __( 'Enable/Disable WooCommerce Checkout Email Validator', 'hs_ev' ),
-                'description' => __('<br/>By enabling it, if a fake email is put an error message will be displayed.','ultimate-sms-notifications'),
+                'label' => __( 'Enable/Disable Email Domain Validator for WooCommerce', 'hs_ev' ),
+                'description' => __('<br/>By enabling it, if a fake email is put an error message will be displayed.','hs_ev'),
                 'required' => true
             ),
             $value
@@ -61,6 +76,11 @@ function hs_ev_settings( ) {
 }
 
 add_action('woocommerce_settings_saved', 'hs_ev_save');
+/**
+ * This function is responsible to save settings.
+ *
+ * @return void
+ */
 function hs_ev_save(){
     if( isset($_POST['enable_hs_ev']) ){
         update_option('hs_ev_enabled',$_POST['enable_hs_ev']);
@@ -70,6 +90,12 @@ function hs_ev_save(){
 }
 
 add_action('woocommerce_checkout_process', 'hs_ev_process');
+
+/**
+ * This function is responsible to process email validation on the checkout.
+ *
+ * @return void
+ */
 function hs_ev_process(){
     $value = get_option('hs_ev_enabled');
     if (isset($value) && isset($_POST['billing_email'])){
@@ -80,7 +106,27 @@ function hs_ev_process(){
     }
 }
 
+/**
+ * This function check if email is valid or not.
+ *
+ * @param string $email
+ * @param string $record
+ * @return bool
+ */
 function hs_ev_check_email($email, $record = 'MX'){
     list($user, $domain) = explode('@', $email);
     return checkdnsrr($domain, $record);
+}
+
+add_action('admin_menu','hs_ev_add_submenu');
+/**
+ * This function is responsible to add a submenu under WooCommerce.
+ */
+function hs_ev_add_submenu(){
+    add_submenu_page('woocommerce',__("Email Domain Validator for WooCommerce",'hs_ev'),__("Email Domain Validator for WooCommerce","hs_ev"),"manage_options","hs_ev","hs_ev_submenu");
+}
+
+function hs_ev_submenu(){
+    wp_redirect('admin.php?page=wc-settings&tab=email&section=hs_ev');
+    exit();
 }
